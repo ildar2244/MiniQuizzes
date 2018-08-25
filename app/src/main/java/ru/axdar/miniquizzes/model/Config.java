ee0133acb0a03d35943fd4d6d2d54ab80c393e66
ildar2244 <ildar2244@gmail.com>
2018-08-25 21:48:38 +0300

    коммит отдельного файла

---
+++ app/src/main/java/ru/axdar/miniquizzes/model/Config.java
@@ -0,0 +1,15 @@
+package ru.axdar.miniquizzes.model;
+
+/**
+ * Created by ildar2244 on 23.08.2018.
+ */
+public class Config {
+
+    public static String TOKEN_FIREBASE = "";
+    public static final String DATABASE_URL = "";
+    public static final String DATABASE_NAME = "";
+    public static final String STORAGE_URL = "";
+
+    public static final String PATH_CATEGORIES = "categories";
+    public static final String PATH_QUIZZES = "";
+}
