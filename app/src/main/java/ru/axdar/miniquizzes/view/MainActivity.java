b7a176bd5433b90b2f8a6e6b61002fac1aa4c218
ildar2244 <ildar2244@gmail.com>
2018-08-26 20:32:39 +0300

    Небольшие изменение в дизайне NavigationView отоборажения списка Категорий.

--- app/src/main/java/ru/axdar/miniquizzes/view/MainActivity.java
+++ app/src/main/java/ru/axdar/miniquizzes/view/MainActivity.java
@@ -1,33 +1,39 @@
 package ru.axdar.miniquizzes.view;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
-import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ru.axdar.miniquizzes.R;
 import ru.axdar.miniquizzes.model.dto.CategoryDTO;
 import ru.axdar.miniquizzes.presenter.NavigationPresenter;
 import ru.axdar.miniquizzes.view.adapter.CategoriesAdapter;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, INavigationView {
+public class MainActivity extends AppCompatActivity implements INavigationView {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.nav_list)
+    RecyclerView recyclerViewCategories;
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
 
     private NavigationPresenter presenterNav;
-    private RecyclerView recyclerViewCategories;
     private CategoriesAdapter categoriesAdapter;
 
 
@@ -35,24 +41,21 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        //navigationView.setNavigationItemSelectedListener(this);
         recyclerViewCategories = findViewById(R.id.nav_list);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         recyclerViewCategories.setLayoutManager(llm);
         categoriesAdapter = new CategoriesAdapter(new ArrayList<>());
         recyclerViewCategories.setAdapter(categoriesAdapter);
 
-        //read more my code
         presenterNav = new NavigationPresenter(this);
         presenterNav.showCategories();
     }
@@ -65,42 +68,15 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
     @Override
     public void showCategories(List<CategoryDTO> vo) {
-        Log.d("MyTAG", "LIST_CAT: " + vo);
         categoriesAdapter.addCategoriesToAdapter(vo);
     }
 
